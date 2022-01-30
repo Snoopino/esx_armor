7796258e3b09183a9d2104168326284eb8765f1b
Musiker15 <info@musiker15.de>
2022-01-30 18:47:24 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,10 @@
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
 	('bulletproof', 'Bulletproof Vest', 1, 0, 1),
 	('bulletproof2', 'Bulletproof Vest', 1, 0, 0),
-    ('bulletproofpolice', 'Bulletproof Vest', 1, 0, 1),
+    ('bulletproofpolice', 'Bulletproof Vest', 1, 0, 1)
+;
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
 	('noarmor', 'Bulletproof Vest', 1, 0, 0),
     ('noarmor2', 'Bulletproof Vest', 1, 0, 1),
 	('noarmorpolice', 'Bulletproof Vest', 1, 0, 0)
