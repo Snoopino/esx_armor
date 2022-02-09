dbdb978e7b504c532f5a7b88d25d74a89d49e37c
Musiker15 <info@musiker15.de>
2022-02-09 20:09:18 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -12,7 +12,7 @@ RegisterNetEvent('esx_armor:setJoinArmor')
 AddEventHandler('esx_armor:setJoinArmor', function(health, armour)
     local playerPed = PlayerPedId()
 
-    Citizen.Wait(10000) -- Please Do Not Touch!
+    Citizen.Wait(1000 * 10) -- Please Do Not Touch! // Set to 60 if you use Multichar.
     SetEntityHealth(playerPed, tonumber(health))
     SetPedArmour(playerPed, tonumber(armour))
 
